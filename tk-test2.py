40edcb9baced225ff4810d9e82f4d47c427d2e4c
ogawa <ocmikanmgmg@gmail.com>
2024-07-09 08:58:43 +0900

    tkinter

--- tk-test2.py
+++ tk-test2.py
@@ -5,15 +5,18 @@ root = tk.Tk()
 root.title("notify system")
 root.geometry("300x200")
 
-frame = tk.Frame(root)
-frame.grid(column=0, row=0)
 
-label = tk.Label(frame, text="enter notification")
-entry = tk.Entry(frame)
-button = tk.Button(frame, text="SEND")
+label_token = tk.Label(text="enter your token")
+entry_token = tk.Entry(width=26)
+label_notify = tk.Label(text="enter notification")
+entry_text = tk.Text(width=30,height=5)
+button = tk.Button(text="SEND",width=10)
 
-label.grid(row=0, column=0)
-entry.grid(row=1, column=0)
-button.grid(row=2, column=0)
+label_token.place(x=55,y=15)
+entry_token.place(x=55,y=40)
+label_notify.place(x=55,y=65)
+entry_text.place(x=55,y=87)
+button.place(x=105,y=165)
+
 
 root.mainloop()
